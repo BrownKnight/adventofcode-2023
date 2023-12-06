7424fe6d8528f8151adf5ab1c7c18d06f723e476
BrownKnight <amanrk@hotmail.co.uk>
2023-12-06 20:00:47 +0000

    Day 6 full solution (31 ms on part 2)

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -50,5 +50,39 @@ public class Day06 : BaseDay
         return new(runningTotal.ToString());
     }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        var totalTime = long.Parse(_input[0].Split(':')[1].Trim().Replace(" ", string.Empty));
+        var recordDistance = long.Parse(_input[1].Split(':')[1].Trim().Replace(" ", string.Empty));
+
+        // Find the first holdTime where the recordDistance is beat
+        var minHoldTime = 0L;
+        for (var holdTime = 0; holdTime < totalTime; holdTime++)
+        {
+            var timeToMove = totalTime - holdTime;
+            var speed = holdTime;
+            var distanceTravelled = timeToMove * speed;
+            if (distanceTravelled > recordDistance)
+            {
+                minHoldTime = holdTime;
+                break;
+            }
+        }
+
+        // do the same but in reverse to find the highest amount of holdTime where the record is beat
+        var maxHoldTime = 0L;
+        for (var holdTime = totalTime; holdTime > minHoldTime; holdTime--)
+        {
+            var timeToMove = totalTime - holdTime;
+            var speed = holdTime;
+            var distanceTravelled = timeToMove * speed;
+            if (distanceTravelled > recordDistance)
+            {
+                maxHoldTime = holdTime;
+                break;
+            }
+        }
+
+        return new((maxHoldTime - minHoldTime + 1).ToString());
+    }
 }
