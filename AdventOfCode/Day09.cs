b86368f3c9f8538c87b9b01a5f54614bc91c8f79
BrownKnight <amanrk@hotmail.co.uk>
2023-12-09 18:11:31 +0000

    Day 09 part 2

--- AdventOfCode/Day09.cs
+++ AdventOfCode/Day09.cs
@@ -41,5 +41,38 @@ public class Day09 : BaseDay
         return new(runningSum.ToString());
     }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        var runningSum = 0;
+        foreach (var line in _input)
+        {
+            var values = line.Split(' ').Select(int.Parse).ToArray();
+            var sequences = new List<int[]>() { values };
+
+            var lastSequence = sequences.Last();
+            while (lastSequence.Any(x => x != 0))
+            {
+                var nextSequence = new int[lastSequence.Length - 1];
+                for (var i = 0; i < nextSequence.Length; i++)
+                {
+                    nextSequence[i] = lastSequence[i + 1] - lastSequence[i];
+                }
+
+                sequences.Add(nextSequence);
+                lastSequence = nextSequence;
+            }
+
+            // Starting from second last sequence, to the first sequence, calculate the new value
+            for (var i = sequences.Count - 2; i >= 0; i--)
+            {
+                var newValue = sequences[i].First() - sequences[i + 1].First();
+                Console.WriteLine($"new first value {newValue}");
+                sequences[i] = [newValue, ..sequences[i]];
+            }
+            Console.WriteLine($"New extrapolated value {sequences.First().First()}");
+            runningSum += sequences.First().First();
+        }
+
+        return new(runningSum.ToString());
+    }
 }
